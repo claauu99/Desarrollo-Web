0e676fb3bc5921183175993687cb90f5892bcde1
claauu99 <claauu99@gmail.com>
2016-05-23 20:07:45 -0400

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -3,6 +3,6 @@
 $link = mysql_connect('localhost', 'root', '')
     or die('No se pudo conectar: ' . mysql_error());
 //echo 'Conexión exitosa';
-mysql_select_db('proyectopractica') or die('No se pudo seleccionar la base de datos');
+mysql_select_db('porton_bdd') or die('No se pudo seleccionar la base de datos');
 mysql_query("SET NAMES 'utf8'");
 ?>
